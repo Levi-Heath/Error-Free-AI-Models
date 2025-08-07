63b6e413b3629f35b40d2ee3702e87660dd25301
Levi Heath <36680223+Levi-Heath@users.noreply.github.com>
2025-08-07 14:00:47 +0000

    Updated read_me.m files for Model_S_h1_m1 and Model_S_h1_m2 to clarify execution steps and remove unnecessary instructions.

--- ImageNet-1k/MATLAB/Model_S_h1_m1/Training_Evaluation/read_me.m
+++ ImageNet-1k/MATLAB/Model_S_h1_m1/Training_Evaluation/read_me.m
@@ -2,10 +2,11 @@
 % 1. Manually create folder: ../Evaluation_Data/Transformed_Data_for_SGD
 % 2. Execute 
 % 		Run_Modularize_Data
+%    in the Data_Transformation folder
 %    to rearrange the training data into modules of labels, and divide 
 %    each module into subsets.
 % (These steps only need to run once.)
 % 3. Execute
-% 	    -- Run_Training_Evaluation_model_1
-%    by choosing the mdoel type, SGD or GDT, from the first line
+% 	    -- Run_Training_Evaluation_model
+%    by choosing the model type, SGD or GDT, from the first line
 %    of the file. 
